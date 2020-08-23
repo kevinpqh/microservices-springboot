2aa1231326b481ebf6896e211bad3641ada8ae0d
kevin <kevinxpt27@gmail.com>
2020-08-23 14:58:27 -0500

    readme y otras mas

---
+++ README.md
@@ -0,0 +1,10 @@
+# Microservices con Spring bbot
+
+Prueba De Microservices
+
+## Referencias
+
+- Tutorial [link](https://www.youtube.com/playlist?list=PLxy6jHplP3Hi_W8iuYSbAeeMfaTZt49PW).
+
+
+
---
+++ service-admin/src/test/java/com/kpq/store/admin/ServiceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpq.store.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
